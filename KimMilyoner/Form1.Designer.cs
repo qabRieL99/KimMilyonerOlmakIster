cae32cc94da7897e51b4d4c511357c1599cb3238
qabRieL99 <mehmetaliumac@gmail.com>
2020-05-10 15:58:53 +0300

    Add project files.

---
+++ KimMilyoner/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace KimMilyoner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5050 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelQuestionNo = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.tanidiaSor = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.labelQustion = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(27, 77);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(140, 29);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(27, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(140, 29);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(27, 116);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(140, 29);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(27, 155);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(140, 29);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(133, 299);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(610, 214);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose one answer";
+            // 
+            // button5050
+            // 
+            this.button5050.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.button5050.Location = new System.Drawing.Point(790, 285);
+            this.button5050.Name = "button5050";
+            this.button5050.Size = new System.Drawing.Size(132, 68);
+            this.button5050.TabIndex = 13;
+            this.button5050.Text = "50/50";
+            this.button5050.UseVisualStyleBackColor = true;
+            this.button5050.Click += new System.EventHandler(this.button5050_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(389, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 68);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "My Final Aswer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelQuestionNo
+            // 
+            this.labelQuestionNo.AutoSize = true;
+            this.labelQuestionNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.labelQuestionNo.ForeColor = System.Drawing.Color.ForestGreen;
+            this.labelQuestionNo.Location = new System.Drawing.Point(12, 541);
+            this.labelQuestionNo.Name = "labelQuestionNo";
+            this.labelQuestionNo.Size = new System.Drawing.Size(70, 25);
+            this.labelQuestionNo.TabIndex = 18;
+            this.labelQuestionNo.Text = "label2";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.labelResult.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.labelResult.Location = new System.Drawing.Point(223, 541);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(60, 25);
+            this.labelResult.TabIndex = 19;
+            this.labelResult.Text = "Time";
+            // 
+            // tanidiaSor
+            // 
+            this.tanidiaSor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.tanidiaSor.Location = new System.Drawing.Point(790, 363);
+            this.tanidiaSor.Name = "tanidiaSor";
+            this.tanidiaSor.Size = new System.Drawing.Size(132, 68);
+            this.tanidiaSor.TabIndex = 20;
+            this.tanidiaSor.Text = "Ask A Friend";
+            this.tanidiaSor.UseVisualStyleBackColor = true;
+            this.tanidiaSor.Click += new System.EventHandler(this.tanidiaSor_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(307, 40);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(281, 90);
+            this.webBrowser1.TabIndex = 16;
+            this.webBrowser1.Visible = false;
+            // 
+            // labelQustion
+            // 
+            this.labelQustion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelQustion.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.labelQustion.Location = new System.Drawing.Point(0, 0);
+            this.labelQustion.Name = "labelQustion";
+            this.labelQustion.Size = new System.Drawing.Size(880, 197);
+            this.labelQustion.TabIndex = 17;
+            this.labelQustion.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelQustion);
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(42, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(880, 197);
+            this.panel1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 575);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tanidiaSor);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelQuestionNo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5050);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Who Wants To Be Millionare? Group B";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button5050;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelQuestionNo;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button tanidiaSor;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label labelQustion;
+        private System.Windows.Forms.Panel panel1;
+
+    }
+}
+
---
+++ KimMilyoner/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Xml.Linq;
+
+
+namespace KimMilyoner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        XElement xQuestions = new XElement("Questions");
+        IEnumerable<XElement> xquestion;
+        int totalQuestions = 0;
+        int currentQuestion = 0;
+        int currectAnswer = 0;
+        string successMessage = string.Empty;
+        string failMessage = string.Empty;
+        bool bloadQuestion = false;
+        int secondremaining = 30;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var qFile = Settings1.Default.ResourceDirectory + "\\QuestionsB.xml";
+            xQuestions = XElement.Load(qFile);
+            currentQuestion = 0;
+            xquestion = from question in xQuestions.Elements("Question")
+                        select question;
+            totalQuestions = xquestion.Count();
+
+            loadQuestion();
+        }
+
+        private void loadQuestion()
+        {
+            try
+            {
+                var curQuesion = xquestion.ElementAt(currentQuestion);
+                labelQuestionNo.Text = string.Format("Question {0}/{1}", currentQuestion + 1, totalQuestions);
+                labelQustion.Text = curQuesion.Attribute("question").Value;
+                if (!string.IsNullOrEmpty(curQuesion.Attribute("photo").Value))
+                {
+                    var photo = Settings1.Default.ResourceDirectory + string.Format("\\{0}", curQuesion.Attribute("photo").Value);
+
+                }
+                else
+                successMessage = curQuesion.Attribute("success").Value;
+                failMessage = curQuesion.Attribute("fail").Value;
+                var xanswer = from question in curQuesion.Elements("answers").Elements("answer")
+                              select question;
+                for (int i = 0; i < 4; i++)
+                {
+                    var controlName = string.Format("checkBox{0}", i + 1);
+                    groupBox1.Controls[controlName].Text = (char)(65 + i) + ". " + xanswer.ElementAt(i).Value;
+                    groupBox1.Controls[controlName].Visible = true;
+                    groupBox1.Controls[controlName].ForeColor = Color.Black;
+                    if (xanswer.ElementAt(i).Attributes().Count() > 0)
+                        currectAnswer = i;
+                }
+                foreach (CheckBox chkbox in groupBox1.Controls)
+                {
+                    chkbox.Checked = false;
+                }
+            }
+            catch
+            {
+                currentQuestion = 0;
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (CheckBox chkbox in groupBox1.Controls)
+            {
+                if (chkbox.Name != (sender as CheckBox).Name)
+                {
+                    chkbox.Checked = false;
+                }
+
+            }
+        }
+
+        private void showResult(string htmlSource)
+        {
+            if (this.webBrowser1.Document == null)
+            {
+                this.webBrowser1.DocumentText = htmlSource;
+            }
+            else
+            {
+                this.webBrowser1.Document.OpenNew(true);
+                this.webBrowser1.Document.Write(htmlSource);
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            bloadQuestion = false;
+            var selectedQuestion = -1;
+            foreach (CheckBox chkbox in groupBox1.Controls)
+            {
+                if (chkbox.Checked)
+                    selectedQuestion = Convert.ToInt32(chkbox.Name.Substring(8));
+
+
+            }
+            if (selectedQuestion != -1)
+            {
+
+
+                if (correctAnswer(selectedQuestion))
+                {
+
+
+
+                    SoundPlayer simpleSound = new SoundPlayer(Settings1.Default.Soundfiles + "\\applause3.wav");
+                    simpleSound.Play();
+                    labelResult.Text = "Correct";
+                    labelResult.ForeColor = Color.Green;
+
+                    showResult(string.Format("<img src='{1}\\congrats.jpg'/><br/><h1  style='color:black'>{0}</h1>", successMessage, Settings1.Default.Soundfiles));
+
+                    currentQuestion++;
+                    if (currentQuestion >= totalQuestions)
+                    {
+
+
+                    }
+                    else
+                    {
+                        bloadQuestion = true;
+                    }
+
+                }
+                else
+                {
+                    labelResult.Text = "Incorrect";
+                    labelResult.ForeColor = Color.Red;
+
+
+                    showResult(string.Format("<img src='{1}\\sorry.jpg'/><br/><h1  style='color:black'>{0}</h1>", failMessage, Settings1.Default.Soundfiles));
+
+                    SoundPlayer simpleSound = new SoundPlayer(Settings1.Default.Soundfiles + "\\explosion_x.wav");
+                    simpleSound.Play();
+                }
+             //   groupBox2.Visible = true;
+
+            }
+            else
+            {
+
+            }
+        }
+     
+        private bool correctAnswer(int selectedAnswer)
+        {
+            if (currectAnswer + 1 == selectedAnswer)
+            {
+                var controlName = string.Format("checkBox{0}", selectedAnswer);
+                groupBox1.Controls[controlName].ForeColor = Color.Green;
+
+
+                return true;
+            }
+            else
+            {
+                var controlName = string.Format("checkBox{0}", selectedAnswer);
+                groupBox1.Controls[controlName].ForeColor = Color.Red;
+                return false;
+            }
+
+
+
+        }
+
+        private void button5050_Click(object sender, EventArgs e)
+        {
+            var incorrectAnswers = new List<int>();
+            foreach (CheckBox chkbox in groupBox1.Controls)
+            {
+
+                var selectedQuestion = Convert.ToInt32(chkbox.Name.Substring(8));
+                if (selectedQuestion != currectAnswer + 1)
+                {
+                    incorrectAnswers.Add(selectedQuestion);
+                }
+            }
+            var randomidx = new Random().Next(0, 2);
+            var randomincorrect = incorrectAnswers[randomidx];
+            foreach (CheckBox chkbox in groupBox1.Controls)
+            {
+                chkbox.Visible = false;
+                var selectedQuestion = Convert.ToInt32(chkbox.Name.Substring(8));
+                if (selectedQuestion == currectAnswer + 1 || selectedQuestion == randomincorrect)
+                {
+                    chkbox.Visible = true;
+
+                }
+            }
+            button5050.Enabled = false;
+            secondremaining += 15;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            secondremaining--;
+
+            if (secondremaining <= -1)
+            {
+                timer1.Enabled = false;
+                SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\tada.wav");
+                simpleSound.Play();
+                MessageBox.Show("Süreniz doldu");
+            }
+            else
+            {
+                showTimeRemaining();
+            }
+
+        }
+
+        private void showTimeRemaining()
+        {
+            System.Windows.Forms.Application.DoEvents();
+            labelResult.Text = secondremaining.ToString();
+
+            showResult("second(s) remaining");
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\Speech Off.wav");
+            simpleSound.Play();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Next question, unnecesary on clicks.
+            //if (currentQuestion < (totalQuestions - 1))
+            //    currentQuestion++;
+            //loadQuestion();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bloadQuestion = false;
+            var selectedQuestion = -1;
+            foreach (CheckBox chkbox in groupBox1.Controls)
+            {
+                if (chkbox.Checked)
+                    selectedQuestion = Convert.ToInt32(chkbox.Name.Substring(8));
+
+
+            }
+            if (selectedQuestion != -1)
+            {
+
+
+                if (correctAnswer(selectedQuestion))
+                {
+
+
+                    timer1.Enabled = false;
+                    SoundPlayer simpleSound = new SoundPlayer(Settings1.Default.Soundfiles + "\\applause3.wav");
+                    simpleSound.Play();
+                    labelResult.Text = "Correct";
+                    labelResult.ForeColor = Color.Green;
+                    
+
+                    showResult(string.Format("<img src='{1}\\congrats.jpg'/><br/><h1  style='color:black'>{0}</h1>", successMessage, Settings1.Default.Soundfiles));
+                    System.Threading.Thread.Sleep(2000);
+
+
+                    if (currentQuestion < (totalQuestions - 1))
+                        currentQuestion++;
+                    labelResult.ForeColor = Color.Black;
+                    loadQuestion();
+                    secondremaining = 30;
+                    timer1.Enabled = true;
+                    if (currentQuestion >= totalQuestions)
+                    {
+    
+                    }
+                    else
+                    {
+                        bloadQuestion = true;
+                    }
+
+                }
+                else
+                {
+                    labelResult.Text = "Incorrect";
+                    labelResult.ForeColor = Color.Red;
+                    timer1.Enabled = false;
+
+                    showResult(string.Format("<img src='{1}\\sorry.jpg'/><br/><h1  style='color:black'>{0}</h1>", failMessage, Settings1.Default.Soundfiles));
+
+                    SoundPlayer simpleSound = new SoundPlayer(Settings1.Default.Soundfiles + "\\explosion_x.wav");
+                    simpleSound.Play();
+                    labelResult.Text = "Yanlış cevap";
+                    System.Threading.Thread.Sleep(2000);
+                    Application.Exit();
+                }
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private void tanidiaSor_Click(object sender, EventArgs e)
+        {
+            secondremaining += 15;
+            timer1.Enabled = true; ;
+            showTimeRemaining();
+            tanidiaSor.Enabled = false;
+        }
+    }
+}
